7589bb4ad9bdd2f78ac5cc7b2116ad990a7d23da
hisham <hisham@localhost>
2020-09-07 19:38:50 +0300

    New method

--- src/main/java/com/hm/spring/boot/docker/demo/SpringBootDockerApplication.java
+++ src/main/java/com/hm/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -3,6 +3,10 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,7 +15,15 @@ public class SpringBootDockerApplication {
 
 	@GetMapping("/message")
 	public String getMessage() {
-		return "Welcome to Hisham Marie REST ..!!! Now ! at night";
+		return "Welcome to Hisham Marie REST ..!!! Now ! at night again";
+	}
+	
+	
+	@RequestMapping(value="user", method = RequestMethod.GET)
+	public @ResponseBody String getItem(@RequestParam("data") String itemid){
+
+	    
+	    return "The value entered is " + itemid;
 	}
 	
 	public static void main(String[] args) {
